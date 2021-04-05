6a13f3eecd15e2e8d255d99b5c43b3a089b330ad
Brown Mamba <brdyherrera@gmail.com>
2021-04-05 02:09:13 -0400

    added questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,13 +7,104 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const questions = [
     {
         type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please enter your project title!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a project description',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please enter a brief summary!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please enter instructions on how to install your project',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please provide instructions!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide information and examples for use',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please provide usage information or enter "N/A"');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please include details on how one can contribute if interested',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please provide contribution guidelines!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Please provide test instructions',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please enter intructions!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
         name: 'github',
         message: 'What is your GitHub username?',
-        validate: githubUser => {
-            if (githubUser) {
+        validate: userInput => {
+            if (userInput) {
                 return true;
             } else {
                 console.log('Please enter your GitHub username!');
+                return false;
+                    }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: userInput => {
+            if (userInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
                 return false;
                     }
         }

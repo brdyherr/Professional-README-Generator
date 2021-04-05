7e80837f44cfe6679aba158d52da587ed1b0f24e
Brown Mamba <brdyherrera@gmail.com>
2021-04-05 01:20:00 -0400

    downloaded dependencies, and added initial question to test the functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+        validate: githubUser => {
+            if (githubUser) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub username!');
+                return false;
+                    }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            console.log(err);
+            return
+        }
+        console.log('Complete!');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    return inquirer.prompt(questions)
+    .then((data) =>{
+        writeToFile('README.md', generateMarkdown(data))
+    })
+}
 
 // Function call to initialize app
 init();
